b62a1f93e2e8421c60480285a20a10a90a8f7faa
hp167 <opriyanka.s123@gmail.com>
2026-07-10 23:04:58 +0530

    Add User schema for authentication middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+{
+    name:{
+        type:String,
+        required:true
+    },
+
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+
+    password:{
+        type:String,
+        required:true
+    },
+
+    profilePic:{
+        type:String,
+        default:""
+    },
+
+    bio:{
+        type:String,
+        default:""
+    },
+
+    followers:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }
+    ],
+
+    following:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }
+    ]
+
+},
+{timestamps:true}
+
+);
+
+module.exports=mongoose.model("User",UserSchema);
